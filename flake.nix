de165befd3bc890e5d29abb6de10313e4b581c7b
Ravi Chandra Padmala <neenaoffline@gmail.com>
2025-12-07 18:54:02 +0530

    Add opencode

--- flake.nix
+++ flake.nix
@@ -3,32 +3,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    aider-nix.url = "github:matko/aider-nix";
     nvf.url = "github:notashelf/nvf";
+    opencode.url = "github:sst/opencode";
   };
 
-  outputs = inputs@{ flake-parts, self, nixpkgs, nvf } : flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ flake-parts, self, nixpkgs, nvf, aider-nix, opencode } : flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" ];
-    perSystem = { pkgs, self', ... }: {
+    perSystem = { pkgs, self', inputs', ... }: {
       packages.zellij = pkgs.zellij;
       packages.neovim = let
         configuration = {};
 
-	customNeovim = nvf.lib.neovimConfiguration {
-          inherit pkgs;
-          modules = [configuration];
-        };
+      customNeovim = nvf.lib.neovimConfiguration {
+        inherit pkgs;
+        modules = [configuration];
+      };
       in
         customNeovim.neovim;
 
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.bashInteractive
-          pkgs.just
-          pkgs.stow
-          pkgs.zellij
-          pkgs.nixfmt-rfc-style
-          self'.packages.neovim
-	];
+            pkgs.just
+            pkgs.stow
+            pkgs.zellij
+            pkgs.zed-editor
+            inputs'.opencode.packages.default
+            pkgs.nixfmt-rfc-style
+            self'.packages.neovim
+        ];
       };
     };
   };
